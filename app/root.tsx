560f43b429f032d4b1301be93ba61a84f18e3468
Noel Espino Córdova <noelespinocordova@tutanota.com>
2022-04-22 13:55:12 -0500

    Add: Route handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,17 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { LiveReload, Outlet } from "@remix-run/react";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+const App = () => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <title>Remix Jokes App</title>
+    </head>
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
+    <body>
+      <Outlet />
+      <LiveReload />
+    </body>
+  </html>
+);
+
+export default App;
---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,11 @@
+const Joke = () => (
+  <div>
+    <h1>Joke</h1>
+
+    <p>Name</p>
+
+    <p>Content</p>
+  </div>
+);
+
+export default Joke;

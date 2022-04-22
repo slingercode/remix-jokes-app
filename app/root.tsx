8d27afebf35e28cb0ad9a3c580a15845f35afc73
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2022-04-22 15:06:41 -0500

    Add: Styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,34 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
+
+import globalStyles from "./styles/global.css";
+import globalMDStyles from "./styles/global-medium.css";
+import globalLGStyles from "./styles/global-large.css";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: globalStyles,
+  },
+  {
+    rel: "stylesheet",
+    href: globalMDStyles,
+    media: "print, (min-width: 640px)",
+  },
+  {
+    rel: "stylesheet",
+    href: globalLGStyles,
+    media: "screen and (min-width: 1024px)",
+  },
+];
 
 const App = () => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
       <title>Remix Jokes App</title>
+
+      <Links />
     </head>
 
     <body>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,29 @@
+import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import styles from "~/styles/index.css";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: styles,
+  },
+];
+
 const Home = () => (
-  <div>
-    <h1>Remix Jokes App</h1>
+  <div className="container">
+    <div className="content">
+      <h1>
+        Remix <span>Jokes!</span>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="jokes">Read Jokes</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 );
 

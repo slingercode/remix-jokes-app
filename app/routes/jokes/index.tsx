1afcb94a896a2d6900137dcfc8847feb198d06de
Noel Espino Córdova <noelespinocordova@tutanota.com>
2022-04-24 14:08:03 -0500

    Add: Prisma Sqlite database

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,6 +1,8 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Link, Outlet } from "@remix-run/react";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
+import { db } from "~/utils/db.server";
 import styles from "~/styles/jokes.css";
 
 export const links: LinksFunction = () => [
@@ -10,39 +12,65 @@ export const links: LinksFunction = () => [
   },
 ];
 
-const Jokes = () => (
-  <div className="jokes-layout">
-    <header className="jokes-header">
-      <div className="container">
-        <h1 className="home-link">
-          <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
-            <span className="logo">🤪</span>
-            <span className="logo-medium">J🤪KES</span>
-          </Link>
-        </h1>
-      </div>
-    </header>
+type LoaderData = {
+  jokes: { id: string; name: string }[];
+};
 
-    <main className="jokes-main">
-      <div className="container">
-        <div className="jokes-list">
-          <Link to=".">Get a random joke</Link>
-          <p>Here are a few more jokes to check out:</p>
-          <ul>
-            <li>
-              <Link to="some-joke-id">Hippo</Link>
-            </li>
-          </ul>
-          <Link to="new" className="button">
-            Add your own
-          </Link>
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokes: await db.joke.findMany({
+      take: 5,
+      select: { id: true, name: true },
+      orderBy: { createdAt: "desc" },
+    }),
+  };
+
+  return json(data);
+};
+
+const Jokes = () => {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
         </div>
-        <div className="jokes-outlet">
-          <Outlet />
+      </header>
+
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">
+            <Link to=".">Get a random joke</Link>
+
+            <p>Here are a few more jokes to check out:</p>
+
+            <ul>
+              {data.jokes.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
+            </ul>
+
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
         </div>
-      </div>
-    </main>
-  </div>
-);
+      </main>
+    </div>
+  );
+};
 
 export default Jokes;
